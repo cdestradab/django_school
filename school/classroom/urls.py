05f6ae7e1c7b610e96f41cfb9253d1ff87825c06
cdestradab <cdestradab@gmail.com>
2023-04-03 01:59:22 -0500

    TemplateView DONE

---
+++ school/classroom/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomeView, ThankYouView
+
+app_name = 'classroom'
+# domain.com/classroom/
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('thank_you', ThankYouView.as_view(), name='thankyou')
+]
