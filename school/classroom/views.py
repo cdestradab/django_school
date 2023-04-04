7739d7d5866fb5442daa4e50b63a217217884560
cdestradab <cdestradab@gmail.com>
2023-04-04 02:23:09 -0500

    FormView DONE

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, FormView
+
+from classroom.forms import ContactForm
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -7,3 +10,15 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thankyou.html'
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    # success URL? URL, NOT A TEMPLATE.html
+    success_url = reverse_lazy('classroom:thankyou')
+
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data) #aquí va la acción que se desea
+        return super().form_valid(form) # esto raramente cambia
